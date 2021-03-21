c3591363ba36351bbaf57f4fee8a325766884141
sreedev <sreedevpadmakumar@gmail.com>
2021-03-22 02:04:25 +0530

    fixed issue with I2C Bus Frequency when converting from 400KHz int to uint

--- include/u8g2_kendryte_hal.h
+++ include/u8g2_kendryte_hal.h
@@ -33,7 +33,7 @@
 #define DEFAULT_DMAC_TX_CHANNEL     DMAC_CHANNEL0
 #define DEFAULT_DMAC_RX_CHANNEL     DMAC_CHANNEL1
 #define DEFAULT_I2C_DEVICE_NUM      I2C_DEVICE_0
-#define OLED_I2C_BUS_DEFAULT_FREQ   400000 /* 4 KHz */
+#define OLED_I2C_BUS_DEFAULT_FREQ   400000u /* 4 KHz */
 
 #define INIT_U8G2_KENDRYTE_HAL(x) u8g2_kendryte_hal_t x = { \
   /* I2C CONFIG */            \
